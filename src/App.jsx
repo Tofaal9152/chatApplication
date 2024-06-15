c01d367cda604f71cf9c6dee40c5725cb3b2c658
Md Tofaal Ahmed <tofaal91522@gmail.com>
2024-06-15 11:55:31 +0600

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 // Adding Style.js
 import style from './style'
+import Navbar from './Components/Navbar'
 
 // App Building
 const App = () => {
   return (
     <div>
-      app
+      <Navbar/>
     </div>
   )
 }
