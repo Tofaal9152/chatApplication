40dfa4febc009abee2c0aff3af7b3d0d496b798d
Md Tofaal Ahmed <tofaal91522@gmail.com>
2024-06-15 11:53:14 +0600

    Starting

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Adding Style.js
+import style from './style'
+
+// App Building
+const App = () => {
+  return (
+    <div>
+      app
+    </div>
+  )
+}
+
+export default App
