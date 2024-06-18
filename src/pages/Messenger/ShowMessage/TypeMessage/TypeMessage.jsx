0ae285cdc3fc52ac39f44924ee007c4255c87a41
Md Tofaal Ahmed <tofaal91522@gmail.com>
2024-06-19 00:52:23 +0600

    Add Firebase hosting and GitHub Actions workflow configuration

--- src/pages/Messenger/ShowMessage/TypeMessage/TypeMessage.jsx
+++ src/pages/Messenger/ShowMessage/TypeMessage/TypeMessage.jsx
@@ -7,43 +7,47 @@ import Emoji from "emoji-picker-react";
 import React, { useState } from "react";
 
 const TypeMessage = () => {
-  const [open_emoji, setopen_emoji] = useState(false);
-  const [text, settext] = useState("");
-  const handleClick = (prev) => {
-    settext((e) => e + prev.emoji);
-    setopen_emoji(false);
+  const [openEmoji, setOpenEmoji] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleClick = (emojiObject) => {
+    setText((prev) => prev + emojiObject.emoji);
+    setOpenEmoji(false);
   };
+
   return (
-    <div className="px-[1rem] custom500:px-[2rem] lg:px-[4rem] xl:px-[7rem] flex  bottom-0  bg-white w-full p-2 overflow-x-hidden dark:bg-[#1e2021] dark:border-solid dark:border-[1px] dark:border-gray-700">
-      <div className="flex justify-center items-center space-x-2 sm:space-x-4 custom500:w-[90vw]  w-full">
-        {/*  */}
-        <span className="cursor-pointer p-1 custom500:p-2 rounded-full hover:bg-gray-100 dark:text-white dark:hover:bg-black">
+    <div className="px-4 flex bottom-0 bg-white w-full p-2 overflow-x-hidden dark:bg-[#1e2021] dark:border-solid dark:border-[1px] dark:border-gray-700">
+      <div className="flex justify-center items-center space-x-2 w-full">
+        {/* Link Icon */}
+        <span className="cursor-pointer p-2 rounded-full hover:bg-gray-100 dark:text-white dark:hover:bg-black">
           <IoLinkOutline size={20} />
         </span>
-        {/*  */}
-        <div className="flex flex-1  items-center bg-white p-1 rounded-full border border-gray-300 dark:bg-[#1e2021]">
-          <span className="cursor-pointer p-1 custom500:p-2 rounded-full hover:bg-gray-100 dark:text-white dark:hover:bg-black">
+        {/* Input Area */}
+        <div className="flex flex-1 items-center bg-white p-1 rounded-full border border-gray-300 dark:bg-[#1e2021]">
+          <span className="cursor-pointer p-2 rounded-full hover:bg-gray-100 dark:text-white dark:hover:bg-black">
             <FaMicrophone size={20} />
           </span>
           <input
             type="text"
             value={text}
-            onChange={(e) => settext(e.target.value)}
+            onChange={(e) => setText(e.target.value)}
             className="flex-1 p-2 text-black text-sm outline-none bg-transparent dark:text-white"
             placeholder="Type a message..."
           />
           <div
-            onClick={() => setopen_emoji((e) => !e)}
-            className="cursor-pointer p-1 custom500:p-2 rounded-full hover:bg-gray-100 dark:text-white dark:hover:bg-black"
+            onClick={() => setOpenEmoji((prev) => !prev)}
+            className="cursor-pointer p-2 rounded-full hover:bg-gray-100 dark:text-white dark:hover:bg-black"
           >
             <BsEmojiSmile size={20} />
           </div>
-          <div className="absolute bottom-[5rem] right-0 p-1 ">
-            <Emoji onEmojiClick={handleClick} open={open_emoji} />
-          </div>
+          {openEmoji && (
+            <div className="absolute bottom-[5rem] right-0 p-1">
+              <Emoji onEmojiClick={handleClick} />
+            </div>
+          )}
         </div>
-        {/*  */}
-        <div className="cursor-pointer p-1 custom500:p-2 rounded-full bg-[#9746ff] hover:bg-[#8e3df8] white">
+        {/* Send Button */}
+        <div className="cursor-pointer p-2 rounded-full bg-[#9746ff] hover:bg-[#8e3df8]">
           <IoIosSend className="text-white" size={22} />
         </div>
       </div>
